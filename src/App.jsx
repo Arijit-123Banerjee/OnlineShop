9febda730dc729183c8baf2437185e5da0d7d390
ArijitBanerjee <arijitbanerjee873@gmail.com>
2024-05-06 21:31:49 +0530

    added payment option

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,7 @@ const App = () => {
   const [cartCount, setCartCount] = useState(0);
   const [cartItems, setCartItems] = useState([]);
   const [calculateCartSum, setCalculateSum] = useState(0);
+  const [theme, setTheme] = useState(false);
 
   const contextValue = {
     dataitem: data,
@@ -43,6 +44,8 @@ const App = () => {
     setCartItems: setCartItems,
     calculateCartSum: calculateCartSum,
     setCalculateSum: setCalculateSum,
+    theme: theme,
+    setTheme: setTheme,
   };
 
   return (
--- src/Pages/Payment.jsx
+++ src/Pages/Payment.jsx
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { shoesContext } from "../Context/ShoeContext";
+import PhonePayIMG from "../assets/PhonePe.png";
+import Card from "../assets/Card.png";
+import { Link } from "react-router-dom";
 const Payment = () => {
+  const { calculateCartSum } = useContext(shoesContext);
+  const { cartItems } = useContext(shoesContext);
+
   return (
     <>
       <div class="font-[sans-serif] bg-white p-4">
@@ -10,42 +17,54 @@ const Payment = () => {
                 <h2 class="text-2xl font-extrabold text-[#333]">
                   Payment method
                 </h2>
+                <div className="w-96 mt-10 border border-gray-300 rounded-md p-4">
+                  <label className="flex items-center space-x-4">
+                    <input type="radio" name="paymentOption" value="phonepay" />
+                    <img
+                      src={PhonePayIMG}
+                      alt="PhonePay"
+                      className="w-6 h-6    "
+                    />
+                    <a href="phonepe://" target="_blank">
+                      <span className="text-sm">PhonePe</span>
+                    </a>
+                  </label>
+                  <label className="flex items-center space-x-4">
+                    <input type="radio" name="paymentOption" value="card" />
+                    <img src={Card} alt="Card" className="w-6 h-6" />
+                    <span className="text-sm">Card</span>
+                  </label>
+                </div>
 
                 <div class="flex flex-wrap gap-4 mt-8">
-                  <button
-                    type="button"
-                    class="min-w-[150px] px-6 py-3.5 text-sm bg-gray-100 text-[#333] rounded-md hover:bg-gray-200"
-                  >
-                    Back
-                  </button>
+                  <Link to={"/checkout"}>
+                    <button
+                      type="button"
+                      class="min-w-[150px] px-6 py-3.5 text-sm bg-gray-100 text-[#333] rounded-md hover:bg-gray-200"
+                    >
+                      Back
+                    </button>
+                  </Link>
                   <button
                     type="button"
                     class="min-w-[150px] px-6 py-3.5 text-sm bg-[#333] text-white rounded-md hover:bg-[#111]"
                   >
-                    Confirm payment $750
+                    Confirm payment {`$ ${calculateCartSum}`}
                   </button>
                 </div>
               </form>
             </div>
             <div class="bg-gray-100 px-6 py-8 rounded-md">
-              <h2 class="text-5xl font-extrabold text-[#333]">$750</h2>
+              <h2 class="text-5xl font-extrabold text-[#333]">{`$ ${calculateCartSum}`}</h2>
               <ul class="text-[#333] mt-10 space-y-6">
-                <li class="flex flex-wrap gap-4 text-base">
-                  Split Sneakers <span class="ml-auto font-bold">$150.00</span>
-                </li>
-                <li class="flex flex-wrap gap-4 text-base">
-                  Echo Elegance <span class="ml-auto font-bold">$200.00</span>
-                </li>
-                <li class="flex flex-wrap gap-4 text-base">
-                  VelvetGlide Boots{" "}
-                  <span class="ml-auto font-bold">$300.00</span>
-                </li>
-                <li class="flex flex-wrap gap-4 text-base">
-                  Tax <span class="ml-auto font-bold">$100.00</span>
-                </li>
-                <li class="flex flex-wrap gap-4 text-base font-bold border-t-2 pt-4">
-                  Total <span class="ml-auto">$750.00</span>
-                </li>
+                {cartItems.map((item) => {
+                  return (
+                    <li class="flex flex-wrap gap-4 text-base">
+                      {item.name}
+                      <span class="ml-auto font-bold">{`$ ${item.price}`}</span>
+                    </li>
+                  );
+                })}
               </ul>
             </div>
           </div>
